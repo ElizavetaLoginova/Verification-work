0c91cdb78290d06728b557bb8ea83ef0eb01fcf4
ElizavetaLoginova <elizyk@mail.ru>
2016-06-01 02:10:34 +0300

    Update Task1.Loginova.cpp

--- Task1.Loginova/Task1.Loginova.cpp
+++ Task1.Loginova/Task1.Loginova.cpp
@@ -3,12 +3,9 @@
 
 using namespace std;
 
-int ContentsFile(char*);
-int* InitArray(char*, int);
-void BubbleSort(int*, int);
-void CreateFile(char*, int, int*);
-int *MergeArrays(int *a, int n, int *b, int m, int& l);
-void Swap(int &a, int &b);
+
+void ShowContentsFile(char*);
+void CreateNewFile(char*, char*, char*);
 
 int main()
 {
@@ -16,123 +13,96 @@ int main()
 	cout << "Enter the name of 1-st file: ";
 	cin >> fileName1;
 	system("cls");
+
 	cout << "Enter the name of 2-nd file: ";
 	cin >> fileName2;
 	system("cls");
+
 	cout << "Enter the name of the resulting file: ";
 	cin >> fileNameResult;
 	system("cls");
 
-	int n = ContentsFile(fileName1);
-	int m = ContentsFile(fileName2);
-	int* Array1 = InitArray(fileName1, n);
-	int* Array2 = InitArray(fileName2, m);
-	int l = 0;
-	int* Result = MergeArrays(Array1, n, Array2, m, l);
-	BubbleSort(Result, l);
-	CreateFile(fileNameResult, l, Result);
-	cout << endl;
+
+	cout << endl << "File 1: ";
+	ShowContentsFile(fileName1);
+	cout << endl << "File 2: ";
+	ShowContentsFile(fileName2);
+
+	CreateNewFile(fileName1, fileName2, fileNameResult);
+	cout << endl << "DONE!";
+
 	system("pause");
-	delete[] Array1;
-	delete[] Array2;
-	delete[] Result;
 	return 0;
 }
 
-int ContentsFile(char* fileName)
+void CreateNewFile(char* fileName1, char* fileName2, char* fileName)
 {
-	ifstream streamIn(fileName);
-	if (!streamIn.is_open())
-	{
-		cout << "Cannot open file to read!" << endl;
-		system("pause");
-		exit(1);
-	}
-	int count = 0, term;
-	while (!streamIn.eof())
-	{
-		streamIn >> term;
-		count++;
-	}
-	streamIn.close();
-	return count;
-}
-int* InitArray(char* fileName, int n)
-{
-	ifstream streamIn(fileName);
-	if (!streamIn.is_open())
-	{
-		cout << "Cannot open file to read!" << endl;
-		system("pause");
-		exit(1);
-	}
-	int* arr = new int[n];
-	for (int i = 0; i < n; i++)
-		streamIn >> arr[i];
-	streamIn.close();
-	return arr;
-}
-
-void BubbleSort(int* arr, int n)//error dimention!
-{
-	for (int i = 1; i <= n - 1; i++)
-	{
-		int max = i - 1;
-		for (int j = i - 1; j <= n - 1; j++)
-		if (arr[j] > arr[max])
-			max = j;
-		Swap(arr[i - 1], arr[max]);
-	}
-}
-void Swap(int &a, int &b)
-{
-	int t = a;
-	a = b;
-	b = t;
-}
-void CreateFile(char* fileName, int n, int* array)
-{
+	ifstream streamIn1(fileName1);
+	ifstream streamIn2(fileName2);
 	ofstream streamOut(fileName);
-	//ofstream streamOut; streamOut.open(fileName, ios::out);
+	if (!streamIn1.is_open())
+	{
+		cout << " Cannot open file " << fileName1 << " to read!" << endl;
+		system("pause");
+		exit(1);
+	}
+	if (!streamIn2.is_open())
+	{
+		cout << " Cannot open file " << fileName2 << " to read!" << endl;
+		system("pause");
+		exit(1);
+	}
 	if (!streamOut.is_open())
 	{
-		cout << "Cannot open file to write!" << endl;
+		cout << " Cannot open file " << fileName << " to write!" << endl;
 		system("pause");
 		exit(1);
 	}
-	int temp;
-	for (int i = 0; i < n; i++)
+
+	system("cls");
+	int a1, a2;
+	streamIn1 >> a1;
+	streamIn2 >> a2;
+	streamOut.width(5);
+	while ((!streamIn1.eof()) && (!streamIn2.eof()))
+	{			
+			if (a1 > a2)
 			{
-				temp = array[i];
-				streamOut.width(5);
-				streamOut << temp;
+				streamOut << a2;
+				streamIn2 >> a2;
 			}
+			
+			else
+			{
+				streamOut << a1;
+				streamIn1 >> a1;
+			}
+	}
+	streamOut << a1;
+
+	streamIn1.close();
+	streamIn2.close();
 	streamOut.close();
+	return;
 }
 
-int *MergeArrays(int *a, int n, int *b, int m, int& l)
+
+
+void ShowContentsFile(char* fileName)
 {
-	l = n + m;
-	int *c = new int[l];
-	int i = 0, j = 0, k = 0;
-	for (; i < n && j < m; k++)
+	ifstream streamIn;
+	streamIn.open(fileName);
+	if (!streamIn.is_open())
 	{
-		if (a[i] < b[j])
-		{
-			c[k] = a[i];
-			i++;
-		}
-		else
-		{
-			c[k] = b[j];
-			j++;
-		}
+		cout << " Cannot open file " << fileName << " to read!" << endl;
+		system("pause");
+		exit(1);
 	}
-	if (i == n)
-	for (; j < m; j++, k++)
-		c[k] = b[j];
-	else
-	for (; i < n; i++, k++)
-		c[k] = a[i];
-	return c;
+	char String[20] = "";
+	while (!streamIn.eof())
+	{
+		streamIn.getline(String, 20, '\n');
+		cout << String << endl;
+	}
 }
+
